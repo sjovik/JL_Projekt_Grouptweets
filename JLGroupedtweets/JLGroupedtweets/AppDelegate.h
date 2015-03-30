448af76b58264e038b4a71b57bb333767a8848e5
Johannes <joh.lilja@gmail.com>
2015-03-30 13:25:10 +0200

    new project

---
+++ JLGroupedtweets/JLGroupedtweets/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  JLGroupedtweets
+//
+//  Created by Johannes on 2015-03-28.
+//  Copyright (c) 2015 Johannes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ JLGroupedtweets/JLGroupedtweets/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JLGroupedtweets
+//
+//  Created by Johannes on 2015-03-28.
+//  Copyright (c) 2015 Johannes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
