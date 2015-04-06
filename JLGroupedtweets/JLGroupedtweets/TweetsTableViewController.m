a9d4c6a2a5ca1cf38c2d140b5acd2ce2c48936fb
Johannes <joh.lilja@gmail.com>
2015-04-06 12:59:40 +0200

    clean och visar text på senaste tweet

--- JLGroupedtweets/JLGroupedtweets/TweetsTableViewController.m
+++ JLGroupedtweets/JLGroupedtweets/TweetsTableViewController.m
@@ -49,12 +49,6 @@
     self.tweetManager = [[JLITweetManager alloc] init];
     self.tweetManager.delegate = self;
     [self.tweetManager fetchTimeline];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -66,12 +60,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return 1;
+    return self.tweetGroups.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return self.tweetGroups.count;
+    return 1;
 }
 
 
@@ -79,58 +73,17 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetsGroup" forIndexPath:indexPath];
     
-    JLITweet *tweetGroup = self.tweetGroups[indexPath.row];
+    NSLog(@"%@", ((JLITweet*)self.tweetGroups[indexPath.section]).author);
+    NSLog(@"%@", self.tweetManager.tweetsByAuthor[((JLITweet*)self.tweetGroups[indexPath.section]).author]);
+    
+    JLITweet *tweetGroup = [self.tweetManager.tweetsByAuthor[((JLITweet*)self.tweetGroups[indexPath.section]).author] lastObject];
     cell.textLabel.text = tweetGroup.author;
+    cell.detailTextLabel.text = tweetGroup.text;
     if (tweetGroup.colorString) {
         cell.backgroundColor = [UIColor colorwithHexString:tweetGroup.colorString];
     }
     
     return cell;
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
